de9fc8638bfe930f25e826545428ba92f346c9ef
Florian Giesemann <fgiesemann@proton.me>
2026-07-13 17:50:41 +0200

    Setup Magic Bitboard Generator tool

---
+++ magic_bitboard_generator/main.cpp
@@ -0,0 +1,10 @@
+/* ************************************************************************** *
+ * Chess Tools - Magic Bitboard Generator                                     *
+ * Search for magic numbers                                                   *
+ * ************************************************************************** */
+
+#include <iostream>
+
+auto main(int argc, const char *argv[]) -> int {
+    std::cout << "Magic Bitboard Generator\n";
+}
