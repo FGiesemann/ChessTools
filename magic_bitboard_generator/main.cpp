9c2dca2d87457c595a49f4d656bed2829b5d648d
Florian Giesemann <fgiesemann@proton.me>
2026-07-22 15:11:41 +0200

    Clean up output

--- magic_bitboard_generator/main.cpp
+++ magic_bitboard_generator/main.cpp
@@ -9,29 +9,33 @@
 
 auto print_search_status(const SearchResult &result) -> void {
     const auto stats = result.generator_result;
-    std::cout << "Statistics:\n";
-    std::cout << std::format("  Stored entries: {} / {}", stats.stored_entries, stats.expected_entries);
-    if (stats.expected_entries != 0) {
-        std::cout << std::format(" = {:.2f} %", static_cast<double>(stats.stored_entries) * 100.0 /
-                                                    static_cast<double>(stats.expected_entries));
-    }
-    std::cout << std::format("\n  Max index: {}\n", stats.max_index);
-    std::cout << std::format("  Constructive collisions: {}\n", stats.constructive_collisions);
-    if (stats.collision_index.has_value()) {
-        std::cout << std::format("  Collision at index: {}\n", stats.collision_index.value());
+    if (stats.successful()) {
+        std::cout << std::format(
+            "Magic number: 0x{:016x} | Index bits: {} | Max index: {} | Constr. coll.: {} | Tries: {}\n",
+            result.magic_number, 64 - result.shift, stats.max_index, result.generator_result.constructive_collisions,
+            result.tries);
     } else {
-        std::cout << std::format("  Tries: {}\n", result.tries);
-        std::cout << std::format("Magic number: 0x{:016x} | Index bits: {}\n", result.magic_number, 64 - result.shift);
+        std::cout << std::format("  Stored entries: {} / {}", stats.stored_entries, stats.expected_entries);
+        if (stats.expected_entries != 0) {
+            std::cout << std::format(" = {:.2f} %", static_cast<double>(stats.stored_entries) * 100.0 /
+                                                        static_cast<double>(stats.expected_entries));
+        }
+        std::cout << std::format("\n  Max index: {}\n", stats.max_index);
+        std::cout << std::format("  Constructive collisions: {}\n", stats.constructive_collisions);
+        std::cout << std::format("  Collision at index: {}\n", stats.collision_index.value());
     }
 }
 
 auto main(int argc, const char *argv[]) -> int {
-    std::cout << "Magic Bitboard Generator\n";
+    const TableSpec table_spec{.piece = chesscore::PieceType::Rook, .square = chesscore::Square::A1};
 
-    MagicBitboardGenerator generator{TableSpec{.piece = chesscore::PieceType::Rook, .square = chesscore::Square::E4}};
+    std::cout << "Magic Bitboard Generator for " << chesscore::to_string(table_spec.piece) << " at "
+              << to_string(table_spec.square) << '\n';
+
+    MagicBitboardGenerator generator{table_spec};
     generator.set_progress_callback(print_search_status);
 
-    const auto result = generator.search({.max_tries = 10'000'000, .shifts = make_shift_range(64 - 12, 64 - 10)});
+    const auto result = generator.search({.max_tries = 10'000'000, .shifts = make_shift_range(64 - 14, 64 - 10)});
 
     print_search_status(result);
 }
