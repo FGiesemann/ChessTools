0b63dffedd5a6da9aac95490446a952ac8cc817d
Florian Giesemann <fgiesemann@proton.me>
2026-07-25 14:58:06 +0200

    Prepare check scenario

---
+++ magic_bitboard_generator/check_scenario.cpp
@@ -0,0 +1,8 @@
+/* ************************************************************************** *
+ * Chess Tools - Magic Bitboard Generator                                     *
+ * Search for magic numbers                                                   *
+ * ************************************************************************** */
+
+#include "check_scenario.h"
+
+namespace magic_bitboard_generator::check_scenario {}
---
+++ magic_bitboard_generator/check_scenario.h
@@ -0,0 +1,11 @@
+/* ************************************************************************** *
+ * Chess Tools - Magic Bitboard Generator                                     *
+ * Search for magic numbers                                                   *
+ * ************************************************************************** */
+
+#ifndef MAGIC_BITBOARD_GENERATOR_CHECK_SCENARIO_H
+#define MAGIC_BITBOARD_GENERATOR_CHECK_SCENARIO_H
+
+namespace magic_bitboard_generator::check_scenario {}
+
+#endif
