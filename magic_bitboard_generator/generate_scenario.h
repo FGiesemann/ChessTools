6a552d0c2940d3dd17f68b5bc800161ec4346c33
Florian Giesemann <fgiesemann@proton.me>
2026-07-30 12:58:44 +0200

    Add header generation

--- magic_bitboard_generator/generate_scenario.h
+++ magic_bitboard_generator/generate_scenario.h
@@ -8,10 +8,30 @@
 
 #include "arg_parse.h"
 
+#include "database.h"
+
+#include <filesystem>
+#include <fstream>
+#include <string>
+
 namespace magic_bitboard_generator::generate_scenario {
 
 auto generate_headers(const Args &args) -> void;
 
-}
+class HeaderFile {
+public:
+    HeaderFile(const std::filesystem::path &out_path) : m_file{out_path} {}
+
+    auto write_header() -> void;
+    auto write_data_set(const std::string &piece_name, const std::vector<Record> &records) -> void;
+    auto start_data_set(const std::string &piece_name) -> void;
+    auto write_record(const Record &record) -> void;
+    auto end_data_set() -> void;
+    auto write_footer() -> void;
+private:
+    std::ofstream m_file;
+};
+
+} // namespace magic_bitboard_generator::generate_scenario
 
 #endif
